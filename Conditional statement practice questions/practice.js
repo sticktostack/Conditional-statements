03e59777d5c107945915be5aa77f04d0dbb002da
sticktostack <rajdeep6153@gmail.com>
2024-01-28 22:39:52 +0530

    first commit

---
+++ Conditional statement practice questions/practice.js
@@ -0,0 +1,82 @@
+// Write a program that determines whether a given number is positive or negative.
+// let a = -10 ;
+//   if(a>0){
+//     console.log("positve");
+//   }else{
+//     console.log("negetuve");
+//   }
+
+// Write a program that checks if a number is even or odd.
+// let a = 13;
+// if(a % 2 === 0){
+//   console.log(`${a} is an even number`);
+// }else{
+//   console.log(`${a} is an odd number`);
+// }
+
+// Write a program to determine the greater of two numbers.
+// let a = 50;
+// let b = 500;
+// if(a>b){
+//   console.log(`${a} is greater than ${b}`);
+// }else{
+//   console.log(`${b} is greater than ${a}`);
+// }
+
+// Write a program that transforms a numerical grade to a letter grade (e.g grade 10 should display “A”).
+// let marks = 10;
+// if(marks < 10){
+//   console.log(`${marks} means grade B`);
+// }else if(marks > 10){
+//   console.log(`${marks} means grade A+`)
+// }else{
+//   console.log(`${marks} means grade A`)
+// }
+
+// Write a program that calculates the ticket price based on age with the following conditions:
+//  age below 12 pay a ticket price of 5, age below 18 pay a ticket price of 10, age below 60 pay a ticket price of 20, age over 60 play a ticket price of 15.
+// let age = prompt("enter your age")
+// if(age < 12){
+//   console.log("Your ticket price is INR 5");
+// }else if(age < 18){
+//   console.log("Your ticket price is INR 10");
+// }else if(age < 60){
+//   console.log("Your ticket price is INR 20");
+// }else{
+//   console.log("Your ticket price is INR 15");
+// }
+
+// Write a program that determines if a year is a leap year.
+// let year = prompt("Check the year is leap year or not !")
+
+// if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+//   console.log(`${year} is a leap year`);
+// }else{
+//   console.log(`${year} is not an leap year`);
+// }
+
+// Write a program that calculates a discount based on the purchase amount.Prices equal or over 100 discount have a discount of 20. Prices equal or over 50 have a discount of 10. Otherwise discount is 0
+// let purchaseAmount = prompt("See your discount")
+// if(purchaseAmount > 100){
+//   console.log("your final amount is "+ purchaseAmount *20/100);
+// }else if(purchaseAmount > 50){
+//   console.log("your final amount is "+ purchaseAmount *10/100);
+// }else{
+//   console.log("your final amount is "+ purchaseAmount + " shop more than 50 & 100 for the discount of 10% and 20%");
+// }
+
+// Write a program that greets the user based on the time of day. Display good morning, good afternnon or good evening based on the time of day when you run the code.
+// let date = new Date()
+// let hour = date.getHours()
+// if(hour <= 5 && hour < 12){
+//   console.log("Good morning");
+// }else if(hour >= 12 && hour < 16){
+//   console.log("Good afternoon");
+// }else if(hour >= 16 && hour < 20){
+//   console.log("Good evening");
+// }else if(hour >= 20 && hour <23){
+//   console.log("Good night");
+// }
+
+let a = matheart
+console.log(a);
